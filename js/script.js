9c63e046d11db2b1b8e75133e2669ba350e4476b
Mateusz Nowicki <m.nowicki941@gmail.com>
2022-12-04 21:28:09 +0100

    Update JS code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
-let button = document.querySelector(".js-navigation__button");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".js-navigation__theme");
-let headerText = document.querySelector(".js-header__text");
-let headerImage = document.querySelector(".js-header__image");
+{
+    const welcome = () => {
+        console.log("Cześć ;)"); 
+    };
+    
+    
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        const headerText = document.querySelector(".js-header__text");
+        const headerImage = document.querySelector(".js-header__image");
+        const themeName = document.querySelector(".js-navigation__theme");
 
-button.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    headerText.classList.toggle("header__text--dark");
-    headerImage.classList.toggle("header__image--dark");
+        body.classList.toggle("dark");
+        headerText.classList.toggle("header__text--dark");
+        headerImage.classList.toggle("header__image--dark");
+    
+        if (body.classList.contains("dark")) {
+            themeName.innerText = "jasny";
+        } else {
+            themeName.innerText = "ciemny";
+        }
+    };
+    
+    const init = () => {
+    const button = document.querySelector(".js-navigation__button");
 
-    if (body.classList.contains("dark")) {
-        themeName.innerText = "jasny";
-    } else {
-        themeName.innerText = "ciemny";
-    }
-});
+    button.addEventListener("click", toggleBackground);
+
+    welcome();
+    };
+
+    init();
+        
+}
