e8dcd1e7d09c91435217ecededd28f90e6e57c0c
Mateusz Nowicki <m.nowicki941@gmail.com>
2024-02-24 17:54:40 +0100

    init  function added

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
-const welcome = () => {
-  console.log("Cześć!");
-};
+{
+  const welcome = () => {
+    console.log("Cześć!");
+  };
 
-let button = document.querySelector(".js-changeButton");
-let delteButton = document.querySelector(".js-delteButton");
+  const toggleBackground = () => {
+    const body = document.querySelector(".js-body");
+    const themeName = document.querySelector(".js-themeName");
 
-const toggleBackground = () => {
-  const body = document.querySelector(".js-body");
-  const themeName = document.querySelector(".js-themeName");
+    body.classList.toggle("body--dark");
+    body.classList.contains("body--dark") ? (themeName.innerText = "jasne") : (themeName.innerText = "ciemne");
+  };
 
-  body.classList.toggle("body--dark");
-  body.classList.contains("body--dark") ? (themeName.innerText = "jasne") : (themeName.innerText = "ciemne");
-};
+  const deltePhotos = () => {
+    const catPhotos = document.querySelector(".js-catPhotos");
+    const delteButton = document.querySelector(".js-delteButton");
 
-const deltePhotos = () => {
-  let catPhotos = document.querySelector(".js-catPhotos");
+    catPhotos.classList.toggle("js-deltedCatPhotos");
+    catPhotos.classList.contains("js-deltedCatPhotos") ? (delteButton.innerText = "Przywróć zdjęcia") : (delteButton.innerText = "Usuń kocie zdjęcia");
+  };
 
-  catPhotos.classList.toggle("js-deltedCatPhotos");
-  catPhotos.classList.contains("js-deltedCatPhotos") ? (delteButton.innerText = "Przywróć zdjęcia") : (delteButton.innerText = "Usuń kocie zdjęcia");
-};
+  const init = () => {
+    const button = document.querySelector(".js-changeButton");
+    button.addEventListener("click", toggleBackground);
 
-welcome();
+    const delteButton = document.querySelector(".js-delteButton");
+    delteButton.addEventListener("click", deltePhotos);
+
+    welcome();
+  };
+
+  init();
+}
