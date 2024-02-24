dd29486ce975f5a4ac109ebc4ddb12d785ca3086
Mateusz Nowicki <m.nowicki941@gmail.com>
2024-02-24 17:32:39 +0100

    toggleBackground function added

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 const welcome = () => {
   console.log("Cześć!");
-}
+};
 
 let button = document.querySelector(".js-changeButton");
-let body = document.querySelector(".js-body");
-let themeName = document.querySelector(".js-themeName");
+
 let catPhotos = document.querySelector(".js-catPhotos");
 let delteButton = document.querySelector(".js-delteButton");
 
-button.addEventListener("click", () => {
-  body.classList.toggle("body--dark");
+const toggleBackground = () => {
+  const body = document.querySelector(".js-body");
+  const themeName = document.querySelector(".js-themeName");
 
+  body.classList.toggle("body--dark");
   body.classList.contains("body--dark") ? (themeName.innerText = "jasne") : (themeName.innerText = "ciemne");
-});
+};
 
 delteButton.addEventListener("click", () => {
   catPhotos.classList.toggle("js-deltedCatPhotos");
-  catPhotos.classList.contains("js-deltedCatPhotos") ? delteButton.innerText = "Przywróć zdjęcia" : delteButton.innerText = "Usuń kocie zdjęcia";
+  catPhotos.classList.contains("js-deltedCatPhotos") ? (delteButton.innerText = "Przywróć zdjęcia") : (delteButton.innerText = "Usuń kocie zdjęcia");
 });
 
 welcome();
